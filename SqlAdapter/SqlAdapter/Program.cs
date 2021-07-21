d2bcd7d306f8302d4c74dac60b57317a8a402e8a
Amit Gaurav <amitg@anthology.com>
2021-07-21 15:46:42 +0530

    File Added

---
+++ SqlAdapter/SqlAdapter/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SqlAdapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString;
+            SqlConnection connection;
+            connectionString = @"Data Source=DESKTOP-QS5GLOC\SQLEXPRESS;Initial Catalog=HRDB;Integrated Security=true";
+            connection = new SqlConnection(connectionString);
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataSet ds = new DataSet();
+
+            string sql = "select * from countries";
+
+            try
+            {
+                connection.Open();
+                SqlCommand SqlCmd = new SqlCommand(sql, connection);
+
+                adapter.SelectCommand = SqlCmd;
+
+                adapter.Fill(ds);
+
+                foreach(DataRow dr in ds.Tables[0].Rows) 
+                {
+                    Console.WriteLine(dr.ItemArray[0] + "---" + dr.ItemArray[1]+"---"+dr.ItemArray[2]);
+                }
+
+                SqlCmd.Dispose();
+
+                 
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Can not open connection!");
+            }
+            finally{
+                adapter.Dispose();
+               
+               connection.Close();
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ SqlAdapterStoredProcedure/SqlAdapterStoredProcedure/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SqlAdapterStoredProcedure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString;
+            connectionString = @"Data Source=DESKTOP-QS5GLOC\SQLEXPRESS;Initial Catalog=HRDB;Integrated Security=true";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            DataSet ds = new DataSet();
+
+            string sql = "Select * from regions";
+
+            //SqlCommand cmd = new SqlCommand(sql, connection);
+            SqlDataAdapter da = new SqlDataAdapter();
+            try
+            {
+                connection.Open();
+                da.SelectCommand = new SqlCommand(sql, connection);
+                da.SelectCommand.CommandType = CommandType.Text;
+               // da.SelectCommand.Parameters.AddWithValue("@OrderId", 1);
+                da.Fill(ds);
+
+                foreach(DataRow dr in ds.Tables[0].Rows)
+                {
+                    Console.WriteLine(dr.ItemArray[0]);
+                }
+
+
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                da.Dispose();
+                connection.Close();
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
