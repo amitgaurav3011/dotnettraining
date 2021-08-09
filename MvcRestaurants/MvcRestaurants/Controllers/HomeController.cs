20d6bdaf896194c2bd31d84f66f46ff8ca708478
Amit Gaurav <amitg@anthology.com>
2021-08-09 17:47:38 +0530

    File added

---
+++ MvcRestaurants/MvcRestaurants/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using MvcRestaurants.Filters;
+using MvcRestaurants.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcRestaurants.Controllers
+{
+    public class HomeController : Controller
+    {
+        MvcRestaurantDb _db = new MvcRestaurantDb();
+        public ActionResult Index()
+        {
+            var model = _db.Restaurants.ToList();
+            return View(model);
+
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            AboutModel ab = new AboutModel();
+            ab.Name = "Anthology";
+            ab.Location = "Bangalore";
+
+            return View(ab);
+        }
+
+        
+       
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            
+
+            return View();
+            
+        }
+    }
+}
---
+++ MvcRestaurants/MvcRestaurants/Filters/LogAttribute.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcRestaurants.Filters
+{
+    public class LogAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+
+        }
+        //
+        // Summary:
+        //     Called by the ASP.NET MVC framework before the action method executes.
+        //
+        // Parameters:
+        //   filterContext:
+        //     The filter context.
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+
+        }
+        //
+        // Summary:
+        //     Called by the ASP.NET MVC framework after the action result executes.
+        //
+        // Parameters:
+        //   filterContext:
+        //     The filter context.
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+
+        }
+        //
+        // Summary:
+        //     Called by the ASP.NET MVC framework before the action result executes.
+        //
+        // Parameters:
+        //   filterContext:
+        //     The filter context.
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+
+        }
+
+    }
+}
